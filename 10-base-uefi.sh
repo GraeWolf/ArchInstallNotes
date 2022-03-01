15285ef493d89c6e573986ac3ac522bedd95612c
Kelly McCuddy <35879312+GraeWolf@users.noreply.github.com>
2022-02-28 19:18:52 -0500

    uncommented the grub lines

--- 10-base-uefi.sh
+++ 10-base-uefi.sh
@@ -22,8 +22,8 @@ pacman -S --noconfirm efibootmgr dialog iwd mtools dosfstools reflector base-dev
 
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-#grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
+grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable cups.service
